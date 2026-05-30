c33e5510978174a65610638a4ccac5dc13fc5072
AceApeiro <info@acestool.com>
2026-05-30 08:48:55 +0530

    Update append.ts

--- api/sheets/append.ts
+++ api/sheets/append.ts
@@ -23,7 +23,7 @@ export default async function handler(
 
     // YOUR APPS SCRIPT WEBHOOK
     const webhookUrl =
-      'https://script.google.com/macros/s/AKfycbzavHfRHlnQcaXGglzV3ogI5x2ylqeGgCu0Ujz5XZHkgBI621MhrKJLIqzo-QbHjIkY/exec';
+      'https://script.google.com/macros/s/AKfycbwcZOnPH8qjplnjEQar4tnIbdnPclpa-detNowoWySTI19kibmA3FzfmqT9rTJ0gRdx/exec';
 
     const response = await fetch(webhookUrl,{
       method:'POST',
